f9a2f376c1a3fa677cbefbf016ac280125f0515f
ysj18614959926 <18614959926@163.com>
2019-07-02 16:41:22 +0800

    打包模式--开发模式(development)+生成模式（production）

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 module.exports = {
   entry: './src/index.js', //打包的入口
+  mode: 'development', //development:开发模式，没有优化速度快--production:生成模式，有优化，速度慢，默认生成模式
   output: {
     path: path.join(__dirname, './dist'), //将打包的结果放到dist目录中
     filename: 'main.js' //自定义打包结果的文件名
